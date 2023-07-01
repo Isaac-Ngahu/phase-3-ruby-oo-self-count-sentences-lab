2237bbee49598254916dcd1e7796e07d33dfcc8b
Isaac-ngahu <isaac.ngahu@student.moringaschool.com>
2023-07-01 15:59:23 +0300

    complete

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,25 @@
 require 'pry'
 
 class String
-
+  attr_accessor :sentence
+  
   def sentence?
-    
+    return self.end_with?(".")
   end
 
   def question?
-
+    return self.end_with?("?")
   end
 
   def exclamation?
-
+    return self.end_with?("!")
   end
 
   def count_sentences
-
+    sentences_array = self.split(/[.!?]+/)
+    sentences_array.count
   end
 end
+sentence = String.new
+sentence.sentence = "This, well, is a sentence. This is too!! And so is this, I think? Woo..."
+sentence.count_sentences
